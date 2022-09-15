a01cd66951c52df041c4ec5051ffc22b3692744f
Eduardo_F_Costa <e.feitosa@spotpromo.com.br>
2022-09-15 20:00:18 -0300

    subindo a primeira aula

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Bem-vindo ao Bytebank")
+    val titular = "Eduardo"
+    val numeroConta = 1000
+    var saldo = 0.0
+
+    saldo = 100.0
+    saldo += 200
+
+    println("Titular: $titular")
+    println("Número da conta: $numeroConta")
+    println("Saldo da conta: $saldo")
+}
